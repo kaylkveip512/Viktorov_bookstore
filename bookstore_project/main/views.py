8b61fe02405024245a70c647843a9933d5e9f803
kaylkveip512 <kaylkveip@gmail.com>
2025-10-07 23:26:28 +0300

    book_store

---
+++ bookstore_project/main/views.py
@@ -0,0 +1,28 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from main.models import Author, Publisher, Genre, Book
+from main.serializers import BookSerializer, AuthorSerializer, PublisherSerializer, GenreSerializer
+
+class AuthorView(APIView):
+    def get(self, request):
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+
+class BookView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+class PublisherView(APIView):
+    def get(self, request):
+        publishers = Publisher.objects.all()
+        serializer = PublisherSerializer(publishers, many=True)
+        return Response(serializer.data)
+
+class GenreView(APIView):
+    def get(self, request):
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        return Response(serializer.data)
